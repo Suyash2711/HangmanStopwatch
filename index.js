4554e56549e94f195aa8a3fab4a2bf3ad89c27ba
Suyash Nigam <71869185+Suyash2711@users.noreply.github.com>
2022-05-22 10:56:46 +0530

    stopwatch created

---
+++ index.js
@@ -0,0 +1,57 @@
+var seconds = 00;
+var tens = 00;
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+var buttonStart = document.querySelector(".startbutton");
+
+var interval;
+
+buttonStart.onclick = function(){
+    clearInterval(interval);
+    interval = setInterval(startTimer, 1000);
+}
+
+function startTimer(){
+    tens++;
+
+    if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+    }
+    if(tens > 9){
+        appendTens.innerHTML = tens;
+    }
+    if(tens > 59){
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens=0;
+        appendTens.innerHTML = "0" + 0;
+    }
+    if(seconds>9){
+        appendSeconds.innerHTML = seconds;
+    }
+    buttonStart.disabled = true;
+}
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------
+
+// var questions = ["What was the official name of the first World Cup?", " In first class Indian matches, the maximum distance (yards) allowed from pitch to the boundary/ line is ?", "Who was ‘the Man of the match’ in the 1983 World Cup final?", "The 1975 World Cup, the first of its kind was played at ?", "Who is the first batsman to cross 10000 runs in Tests ?"];
+// var answers = ["PRUDENTIAL", "75", "KAPIL DEV", "LORDS", "SUNIL GAVASKAR"];
+
+// var random = Math.floor(Math.random()*questions.length);
+
+// var selectedQ = questions[random];
+// var selectedA = answers[random];
+
+// var category = "Cricket";
+
+// var lives=5, live=0, win=0;
+// var totalMistakes = 5;
+
+
