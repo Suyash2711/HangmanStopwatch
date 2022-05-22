3dfb7722ea1ef18f7e7a4237adc0a193c143f435
Suyash Nigam <71869185+Suyash2711@users.noreply.github.com>
2022-05-22 14:27:08 +0530

    correct number of dashes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ buttonStart.onclick = function(){
 }
 
 function startTimer(){
+    
     tens++;
 
     if(tens <= 9){
@@ -31,6 +32,7 @@ function startTimer(){
         appendSeconds.innerHTML = seconds;
     }
     buttonStart.disabled = true;
+    // show();
 }
 
 // ---------------------------STOPWATCH END----------------------------------------
@@ -44,6 +46,8 @@ var answers = ["PRUDENTIAL", "75", "KAPIL DEV", "LORDS", "SUNIL GAVASKAR"];
 
 var random = Math.floor(Math.random()*questions.length);
 
+// var key = 
+
 var selectedQ = questions[random];
 var selectedA = answers[random];
 
@@ -52,26 +56,50 @@ var category = "Cricket";
 var lives=5;
 var totalMistakes = 5;
 
-var space= [];
+var correct= [];
 
 var wrong= [];
 
 // document.getElementById("question").style.display = "none";
 document.getElementById("question").innerHTML = selectedQ;
 document.getElementById("category").innerHTML = category;
+var ans = document.querySelector(".answer");
+var correct_ans = [];
 
-function show(){
-    var element = document.getElementsByClassName(".startbutton");
-    element.classList.add("show");
+
+// function show(){
+//     // var element = document.getElementsByClassName(".startbutton");
+//     // element.classList.add("show");
+
+    
+// }
+var space = [];
+for(var i=0; i<selectedA.length; i++){
+    correct_ans.push(selectedA[i]);
 }
+console.log(correct_ans);
+// ans.innerHTML = correct_ans;
 
-for(var i=0; i<selectedA.length; i++){  
-    if(answers[random][i] == " "){
+for(var i=0; i<selectedA.length; i++){
+    if(correct_ans[i] == ' '){
         space.push("/");
     }
     else{
         space.push("_");
     }
+    space.join(" ");
 }
+console.log(space);
+ans.innerHTML = space.join(" ");
+
+for(var i=0; i<selectedA.length; i++){  
+    if(answers[random][i] == " "){
+        correct.push("/");
+    }
+    else{
+        correct.push(selectedA[i]);
+    }
+}
+console.log(correct); //answer in correct
 
 
